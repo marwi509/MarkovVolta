389b012840e822350acc74fdc59065c414e3793d
marwi509 <marwi509@student.liu.se>
2012-10-02 09:52:18 +0200

    Changed the way file names are added.

--- src/lyricsGenerator/MarkovVolta.java
+++ src/lyricsGenerator/MarkovVolta.java
@@ -21,21 +21,40 @@ public class MarkovVolta {
 		*/
 		
 		FileReader reader = new FileReader();
-		CharParser parser = new CharParser();
+		Parser parser = new CharParser();
+		Sequence.setSequenceLength(4);
 		MarkovDictionary dictionary = new MarkovDictionary();
-		reader.readFile("C:/a/zed.txt");
-		Vector<LyricsItem> vector = parser.parse(reader.getContent());
+		//reader.readFile();
+		//Vector<LyricsItem> vector = parser.parse(reader.getContent());
 		
-		reader.readFile("C:/a/Mars Volta lyrics/inertiaticesp.txt");
-		vector = parser.parse(reader.getContent());
 		
-		reader.readFile("C:/a/Mars Volta lyrics/roulettedares.txt");
-		vector = parser.parse(reader.getContent());
+		//reader.readFile("/home/marcus/Dokument/Mars Volta/goliath.txt");
+		//vector = parser.parse(reader.getContent());
 		
+		//reader.readFile();
+		//vector = parser.parse(reader.getContent());
+		/*
 		reader.readFile("C:/a/Mars Volta lyrics/drunkshipoflanterns.txt");
 		vector = parser.parse(reader.getContent());
+		*/
+Vector<String> Files = new Vector<String>();
 		
-		dictionary.addItemVector(vector);
+		//Files.add(new String("C:/a/bible.txt"));
+		Files.add(new String("/home/marcus/Dokument/Mars Volta/aegis.txt"));
+		Files.add(new String("/home/marcus/Dokument/Mars Volta/goliath.txt"));
+		Files.add(new String("/home/marcus/Dokument/Mars Volta/lvia.txt"));
+		Files.add(new String("/home/marcus/Dokument/Mars Volta/roulette.txt"));
+		Files.add(new String("/home/marcus/Dokument/Mars Volta/esp.txt"));
+		Files.add(new String("/home/marcus/Dokument/Mars Volta/son.txt"));
+		Files.add(new String("/home/marcus/Dokument/Mars Volta/drunkship.txt"));
+		for(int i = 0; i < Files.size(); i ++)
+		{
+			reader.readFile(Files.get(i));
+			Vector<LyricsItem> vector = parser.parse(reader.getContent());
+			dictionary.addItemVector(vector);
+		}
+		
+		//dictionary.addItemVector(vector);
 		LyricsCreator Cedric = new LyricsCreator();
 		Cedric.setInput(dictionary);
 		System.out.println(Cedric);
