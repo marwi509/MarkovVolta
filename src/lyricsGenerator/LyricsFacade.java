a3a2b2122a59f5e78248dca85cc073fef615c911
marwi509 <marwi509@student.liu.se>
2012-10-06 15:07:21 +0200

    Added the most important private variables.

--- src/lyricsGenerator/LyricsFacade.java
+++ src/lyricsGenerator/LyricsFacade.java
@@ -1,5 +1,14 @@
 package lyricsGenerator;
 
+/**
+ * 
+ * @author Marcus Widegren
+ *	LyricsFacade is the main interface for the lyricsCreator package.
+ */
+
 public class LyricsFacade {
+	private Parser theParser;
+	private Dictionary theDictionary;
+	private LyricsCreator theLyricsCreator;
 	
 }
