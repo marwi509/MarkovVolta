fe4a31dde34e7c4e98dd5ad6cee8fe488350f1d7
marwi509 <marwi509@z-app.se>
2012-10-14 14:44:30 +0200

    Added an arraytable implementing the table interface. Mainly there for benchmarking the hashtable

---
+++ src/markov/util/ArrayTable.java
@@ -0,0 +1,38 @@
+package markov.util;
+
+import java.util.Vector;
+
+public class ArrayTable<Element extends Copyable & Comparable> implements Table<Element>{
+	private Vector<Element> theList;
+	
+	public ArrayTable()
+	{
+		theList = new Vector<Element>();
+	}
+	
+	@Override
+	public Element contains(Element theElement) {
+		for(int i = 0; i < theList.size(); i ++)
+		{
+			if(theElement.equals(theList.get(i)))
+			{
+				return theList.get(i);
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public Element insert(Element theElement) {
+		for(int i = 0; i < theList.size(); i ++)
+		{
+			if(theElement.equals(theList.get(i)))
+			{
+				return theList.get(i);
+			}
+		}
+		theList.add((Element)theElement.copyMe());
+		return theList.get(theList.size()-1);
+	}
+
+}
