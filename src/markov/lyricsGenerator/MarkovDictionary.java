20b2cd6806cdc87861840500e250658e637c6557
marwi509 <marwi509@student.liu.se>
2012-10-16 09:50:44 +0200

    Changed the way markovdictionary works. Now it contains one hashtable only

--- src/markov/lyricsGenerator/MarkovDictionary.java
+++ src/markov/lyricsGenerator/MarkovDictionary.java
@@ -7,75 +7,46 @@ import markov.util.HashTable;
 
 
 public class MarkovDictionary implements Dictionary{
-
-	private Vector<SequenceProb> items = new Vector<SequenceProb>();
 	private Sequence theSequence = new Sequence();
-	private HashTable<Sequence> table = new HashTable<Sequence>(1024 * 16);
-	private HashTable<Pair> theItemTable = new HashTable<Pair>(1024 * 16);
+	private HashTable<SequenceList> theSequenceListTable = new HashTable<SequenceList>(1024 * 1024);
 	private Random randomGenerator = new Random();
 	
 	@Override
-	public void addItemVector(Vector<LyricsItem> theVector) {
-		
+	public void addItemVector(Vector<LyricsItem> theVector) 
+	{
 		theSequence = new Sequence();
 		for(int i=0;i<theVector.size();i++)
 		{
 			if(i % 50000 == 0)
-				System.out.println("Item " + i + " of " + theVector.size());
-			Pair tempPair = theItemTable.contains(new Pair(theVector.get(i), 0));
-			//int alreadyExists = theItemTable.contains(new Pair(theVector.get(i), 0));
-			
-			if(tempPair != null)
-			{
-				items.get(tempPair.getAmount()).addSequence(theSequence);
-			}
-			else
-			{
-				theItemTable.insert(new Pair(theVector.get(i), items.size()));
-				items.add(new SequenceProb(theVector.get(i),theSequence));
-			}
-			Sequence tempSequence = table.insert(theSequence);
-			tempSequence.increment();
+				System.out.println("Item " + i + " of " + theVector.size());	
+			SequenceList theSList = theSequenceListTable.insert(new SequenceList(theSequence));
+			theSList.addItem(theVector.get(i));
 			theSequence.push(theVector.get(i));
 		}
 		theSequence = new Sequence();
 	}
 
 	@Override
-	public LyricsItem getItem() {
-		if(table.contains(theSequence) == null)
+	public LyricsItem getItem() 
+	{
+		SequenceList theSList = theSequenceListTable.contains(new SequenceList(theSequence));
+		if(theSList == null)
 		{
 			theSequence = new Sequence();
+			return getItem();
 		}
-		float totalAmount = 0.0f;
-		Sequence tempSequence = table.contains(theSequence);
-		if(tempSequence != null)
-		{
-			totalAmount = (float)tempSequence.getAmount();
-		}
-		float randomNumber = randomGenerator.nextFloat();
-		float lowerLimit = 0;
-		float upperLimit = (float)items.elementAt(0).getAmount(theSequence)/totalAmount;
+		int randNumber = randomGenerator.nextInt(theSList.insertions());
 		
-		for(int i=0;i<items.size()-1;i++)
+		for(int i = 0; i < theSList.size(); i ++)
 		{
-		
-			/*System.out.println("lower: "+lowerLimit);
-			System.out.println("random: "+randomNumber);
-			System.out.println("upper: "+upperLimit);
-			*/
-			if(randomNumber > lowerLimit && randomNumber < upperLimit)
-			{	
-				theSequence.push(items.elementAt(i).getItem());
-				return items.elementAt(i).getItem();
+			if(randNumber < theSList.getItemNumber(i).getAmount())
+			{
+				theSequence.push(theSList.getItemNumber(i).getItem());
+				return theSList.getItemNumber(i).getItem();
 			}
-			lowerLimit+=(float)items.elementAt(i).getAmount(theSequence)/totalAmount;
-			upperLimit=lowerLimit+(float)items.elementAt(i+1).getAmount(theSequence)/totalAmount;
-			
-			
 		}
-		theSequence.push(items.lastElement().getItem());
-		return items.lastElement().getItem();
+		theSequence.push(theSList.getItemNumber(theSList.size() - 1).getItem());
+		return theSList.getItemNumber(theSList.size() - 1).getItem();
 	}
 
 }
---
+++ src/markov/lyricsGenerator/SequenceList.java
@@ -0,0 +1,82 @@
+package markov.lyricsGenerator;
+
+import java.util.Vector;
+
+import markov.util.Comparable;
+import markov.util.Copyable;
+import markov.util.HashTable;
+import markov.util.Hashable;
+import markov.util.Table;
+
+public class SequenceList implements Hashable{
+	private Sequence theSequence;
+	private Vector<Pair> theList;
+	private Table<Pair> theListIndices;
+	private int insertions = 0;
+	
+	public SequenceList(Sequence theSequenceIn)
+	{
+		theSequence = theSequenceIn.copyMe();
+		theList = new Vector<Pair>();
+		theListIndices = new HashTable<Pair>(64);
+	}
+	
+	public void addItem(LyricsItem theItem)
+	{
+		insertions++;
+		Pair tempPair = theListIndices.contains(new Pair(theItem, 0));
+		if(tempPair != null)
+		{
+			int index = tempPair.getAmount();
+			theList.get(index).setAmount(theList.get(index).getAmount() + 1);
+		}
+		else
+		{
+			Pair newPair = new Pair(theItem, 1);
+			theList.add(newPair);
+			theListIndices.insert(new Pair(theItem, theList.size() - 1));
+		}
+	}
+	
+	public Pair getItemNumber(int index)
+	{
+		if(index >= size())
+			return null;
+		return theList.get(index);
+	}
+	
+	public int size()
+	{
+		return theList.size();
+	}
+	
+	public int insertions()
+	{
+		return insertions;
+	}
+	
+	public Sequence getSequence()
+	{
+		return theSequence;
+	}
+
+	@Override
+	public Copyable copyMe() {
+		SequenceList sequenceOut = new SequenceList(theSequence);
+		return sequenceOut;
+	}
+
+	@Override
+	public boolean equals(Comparable C) {
+		if(this.getClass() != C.getClass())
+			return false;
+		SequenceList S = (SequenceList) C;
+		return S.getSequence().equals(this.getSequence());
+	}
+	
+	public int hashCode()
+	{
+		return this.getSequence().hashCode();
+	}
+}
+
