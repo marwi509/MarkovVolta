771f31f67863088854be3d95777e441c387106f0
marwi509 <marwi509@student.liu.se>
2015-07-02 16:00:47 +0200

    Changed back to using the GUI

--- src/main/java/markov/main/Main.java
+++ src/main/java/markov/main/Main.java
@@ -4,11 +4,6 @@ import markov.lyricsGenerator.LyricsFacade;
 
 public class Main {
     public static void main(String[] args) {
-        LyricsFacade f = new LyricsFacade();
-        f.addSong("C:\\a\\newbible2.txt");
-        f.addSong("C:\\a\\thongsong.txt");
-        f.addSong("C:\\a\\thunderroad.txt");
-
-        System.out.println(f.generateSong());
+        new MarkovMainFrame();
     }
 }
