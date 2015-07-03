e1ab89d3eb420ddc6a42fa2db583ea8defcff4ea
marwi509 <marcuswidegren123@gmail.com>
2015-07-03 22:50:47 +0200

    Added test for multiple songs as input and cleaned the test code

--- src/test/java/integration/ReferenceGenerator.java
+++ src/test/java/integration/ReferenceGenerator.java
@@ -1,36 +1,24 @@
 package integration;
 
-import markov.lyricsGenerator.LyricsFacade;
 import markov.util.io.FileStringWriter;
 
-import java.util.Random;
+import java.util.Arrays;
 
 public class ReferenceGenerator {
 
-    public final static long seed = 1337;
-    public final static String referenceLocation = "src/test/resources/reference/";
-    private final static String lyricsLocation = "/lyrics/";
-
     public static void main(String[] args) {
-        generateReferenceForSong("goliath");
-        generateReferenceForSong("bohemianrhapsody");
-        generateReferenceForSong("meccamputechture");
-        generateReferenceForSong("thongsong");
-        generateReferenceForSong("thunderroad");
-        generateReferenceForSong("zed");
+        Arrays.stream(TestUtils.songs).forEach(ReferenceGenerator::generateReferenceForSong);
+        generateReferenceForMultipleSongs(TestUtils.songs);
+    }
+
+    private static void generateReferenceForMultipleSongs(String... songNames) {
+        String result = TestUtils.generateSong(songNames);
+        FileStringWriter.toFile(result, TestUtils.referenceFilename(TestUtils.generateFilePrefix(songNames)));
     }
 
     private static void generateReferenceForSong(String songName) {
-        String result = generateSong(songName);
-        FileStringWriter.toFile(result, referenceLocation + songName + "-reference.txt");
-    }
-
-    public static String generateSong(String songName) {
-        final LyricsFacade facade = new LyricsFacade(new Random(seed));
-        facade.setCharacterSequenceLength(4);
-        facade.setUseCharacter();
-        facade.addSong(facade.getClass().getResourceAsStream(lyricsLocation + songName + ".txt"));
-        return facade.generateSong();
+        String result = TestUtils.generateSong(songName);
+        FileStringWriter.toFile(result, TestUtils.referenceFilename(songName));
     }
 
 }
